00a1253f53233654e5e72a16a35d637fbe71d0f7
Yunus AKSU <dev.yunus.aksu@gmail.com>
2019-06-26 15:24:00 +0300

    Mdoel
    .

---
+++ Mesajim.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FikrimVar.Models
+{
+    public class Mesajim
+    {
+        public string MesajBaslik { get; set; }
+        [Required(ErrorMessage = "Bu alan boş geçilemez")]
+        [MinLength(20, ErrorMessage = "İçerik az")]
+        public string MesajIcerik { get; set; }
+        public int KategoriId { get; set; }
+    }
+}
---
+++ UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FikrimVar.Models
+{
+    public class UserModel
+    {
+        public string KullaniciAdi { get; set; }
+        public string KullaniciSoyadi { get; set; }
+        public string Sifre { get; set; }
+    }
+}
